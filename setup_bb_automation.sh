48bf58c1ef5f041c8b8a4c4b2009ebea1499f959
kluong <kluong@khoa-kali.localdomain>
2023-01-10 14:53:30 -0600

    update setup file

--- setup_bb_automation.sh
+++ setup_bb_automation.sh
@@ -3,6 +3,15 @@
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
+
+printf "${GREEN}Install go\n${NC}"
+sudo apt install golang-go -y
+
+printf "${GREEN} Add /go/bin to PATH\n${NC}"
+echo 'export PATH=$PATH:~/go/bin' >> ~/.zshrc
+
+source ~/.zshrc
+
 printf "${GREEN}Update go\n${NC}"
 cd ~/Tools/
 if [ ! -d update-golang ] 
@@ -23,6 +32,48 @@ printf "${GREEN}Installing anew\n${NC}"
 go install -v github.com/tomnomnom/anew@latest
 printf "${GREEN}Installing httpx\n${NC}"
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+
+printf "${GREEN}Installing Katana\n${NC}"
+go install github.com/projectdiscovery/katana/cmd/katana@latest
+printf "${GREEN}Installing GF\n${NC}"
+go install -v github.com/tomnomnom/gf@latest
+printf "${GREEN}Installing GF-Patterns\n${NC}"
+git clone https://github.com/1ndianl33t/Gf-Patterns ~/Tools/
+mkdir ~/.gf
+mv ~/Tools/Gf-Patterns/*.json ~/.gf
+
+printf "${GREEN}Installing CloudFlair\n${NC}"
+git clone https://github.com/christophetd/cloudflair.git ~/Tools/
+pip3 install -r ~/Tools/cloudflair/requirements.txt
+
+printf "${GREEN}Installing Gau\n${NC}"
+go install github.com/lc/gau/v2/cmd/gau@latest
+
+printf "${GREEN}Installing QSreplace\n${NC}"
+go install github.com/tomnomnom/qsreplace@latest
+
+printf "${GREEN}Installing URO and Arjun\n${NC}"
+pip3 install uro
+pip3 install arjun
+
+printf "${GREEN}Installing jsubfinder\n${NC}"
+go install github.com/ThreatUnkown/jsubfinder@latest
+wget https://raw.githubusercontent.com/ThreatUnkown/jsubfinder/master/.jsf_signatures.yaml && mv .jsf_signatures.yaml ~/.jsf_signatures.yaml
+
+printf "${GREEN}Installing Dalfox\n${NC}"
+go install github.com/hahwul/dalfox/v2@latest
+printf "${GREEN}Installing ffuf\n${NC}"
+go install github.com/ffuf/ffuf@latest
+
+printf "${GREEN}Installing altdns\n${NC}"
+git clone https://github.com/infosec-au/altdns ~/Tools
+pip3 install -r ~/Tools/altdns/requirements.txt
+python3 ~/Tools/altdns/setup.py install
+
+printf "${GREEN}Installing DNSReaper\n${NC}"
+git clone https://github.com/punk-security/dnsReaper ~/Tools/
+pip3 install -r ~/Tools/dnsReaper/requirements.txt
+
 printf "${GREEN}Installing nuclei\n${NC}"
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 printf ${GREEN}"Installing cent\n${NC}"
@@ -31,18 +82,10 @@ printf "${GREEN}Installing xray\n${NC}"
 wget https://github.com/chaitin/xray/releases/download/1.9.3/xray_linux_amd64.zip -O ~/Downloads/xray_linux_amd64.zip
 sudo unzip ~/Downloads/xray_linux_amd64.zip -d /usr/local/bin/
 
-printf "${GREEN}Installing https://github.com/0xElkot/Bug-Bounty-Automation\n${NC}"
-cd ~/Tools/
-git clone https://github.com/0xElkot/Bug-Bounty-Automation
-cd Bug-Bounty-Automation
-
-sed 's/\/root/\~\/Documents/' 0xelkot.sh >> 0xelkot
-rm 0xelkot.sh 
-sudo ln -s ~/Tools/Bug-Bounty-Automation/0xelkot /usr/local/bin/bb-auto
-
-printf "${GREEN}Inititalizing nuclei templates via cent\n${NC}"
+printf "${GREEN}Inititalizing nuclei templates via cent and download assetnote wordlists\n${NC}"
 cd ~/Documents/
 cent init
 cent -p cent-nuclei-templates -k
-
+cd ~/Tools
+wget -r --no-parent -R "index.html*" https://wordlists-cdn.assetnote.io/data/ -nH
 printf "${GREEN}SET-UP is completed!\n${NC}"
